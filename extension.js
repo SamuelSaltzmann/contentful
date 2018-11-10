ccf869af2b9f20264e98a2f30163a693dc5ae5d3
Sami Salman <samisalman@Samis-MacBook-Pro.local>
2018-11-10 01:26:15 +0100

    first commit

---
+++ extension.js
@@ -0,0 +1,13 @@
+"extension": {
+	"name": "Advanced required fields",
+		"srcdoc": "<!DOCTYPE html>...",
+		"parameters": {
+			"instance": [
+			{
+				"id": "uniqueReference",
+				"type": "Asset",
+				"name": "Unique reference"
+			}
+		]
+	}
+}
